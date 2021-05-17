762a082917387041f21584b29eb50e1ad01b52af
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-05-17 12:06:25 +0300

    Common katmanı icin sınıflar yaratıldı

---
+++ Project.DAL/StrategyPattern/MyInit.cs
@@ -0,0 +1,29 @@
+﻿using Project.DAL.ContextClasses;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.StrategyPattern
+{
+    //Bogus Kütüphanesi bize hazır Data sunacaktır...
+    public  class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            #region Admin
+
+
+            AppUser au = new AppUser();
+            au.UserName = "cgr";
+            
+
+
+
+            #endregion
+        }
+    }
+}
