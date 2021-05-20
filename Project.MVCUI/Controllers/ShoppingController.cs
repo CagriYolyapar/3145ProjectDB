d898ed3e038457359192ef4399afb261b7bcd116
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-05-20 13:26:14 +0300

    View NameSpace'leri düzenlenderek Pagination geliştirildi

--- Project.MVCUI/Controllers/ShoppingController.cs
+++ Project.MVCUI/Controllers/ShoppingController.cs
@@ -1,4 +1,6 @@
-﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+﻿using PagedList;
+using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.MVCUI.VMClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +24,26 @@ namespace Project.MVCUI.Controllers
             _odRep = new OrderDetailRepository();
         }
         // GET: Shopping
-        public ActionResult ShoppingList()
+
+        //Sayfalama işlemleri yapmak icin Pagination kütüphanesinden yararlanıyoruz(PagedList)
+        public ActionResult ShoppingList(int? page,int? categoryID) //nullable int vermemizin sebebi aslında buradaki int'in sayfa sayımızı temsil edecek olmasıdır...Ancak birisi direkt alısveriş sayfasına ulastıgında sayfa sayısını göndermeyeceginden bu sekilde de Action'in calısabilmesini istiyoruz...
         {
-            return View();
+
+            //string a = "Mehmet";
+            //a = null;
+            //string b = a??"Cagri"; //Eger a null ise b'ye Cagri at ... null degilse b'ye git a'nın degerini at demektir...
+            //page??1
+
+            //Alt tarafta page?? demek page null geliyorsa demektir...
+            PAVM pavm = new PAVM
+            {
+                PagedProducts = categoryID == null? _pRep.GetActives().ToPagedList(page??1,9):_pRep.Where(x=>x.CategoryID==categoryID).ToPagedList(page??1,9),
+                Categories = _cRep.GetActives()
+            };
+
+            if (categoryID != null) TempData["catID"] = categoryID;
+
+            return View(pavm);
         }
     }
 }
