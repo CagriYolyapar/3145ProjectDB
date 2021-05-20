94dcfa1b2d2e9aea212b252697406dde647851c6
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-05-20 13:05:11 +0300

    Shopping List tasarımı entegre edildi

---
+++ Project.MVCUI/Controllers/ShoppingController.cs
@@ -0,0 +1,30 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        OrderRepository _oRep;
+        ProductRepository _pRep;
+        CategoryRepository _cRep;
+        OrderDetailRepository _odRep;
+
+        public ShoppingController()
+        {
+            _oRep = new OrderRepository();
+            _pRep = new ProductRepository();
+            _cRep = new CategoryRepository();
+            _odRep = new OrderDetailRepository();
+        }
+        // GET: Shopping
+        public ActionResult ShoppingList()
+        {
+            return View();
+        }
+    }
+}
